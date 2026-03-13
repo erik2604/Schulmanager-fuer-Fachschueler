fa7ad42210b2a0429c3d0c3c81f1e4ebe4cfd8f0
Pferdusmaximus <nilsbraeunlein@gmail.com>
2026-03-13 10:33:19 +0100

    ende

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def menu():
     return render_template("menue.html")
 
 
+@app.route("/termine")
+def termine():
+    return render_template("termine.html")
+
+
+@app.route("/notenuebersicht")
+def notenuebersicht():
+    return render_template("notenübersicht.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
